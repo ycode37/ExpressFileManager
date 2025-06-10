008036527f2b606f697c9bb7cf3530f01a9b4493
ycode37 <ycode37@gmail.com>
2025-06-10 20:24:12 +0530

    Update user.js

--- FileManager/models/user.js
+++ FileManager/models/user.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://127.0.0.1:27017/FileManager`);
+// Use the environment variable for MongoDB connection
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 const userSchema = mongoose.Schema({
     username: String,
