dd9deadfbee926bb5f8229b2f1a17b61cf570ebd
ycode37 <ycode37@gmail.com>
2025-06-10 21:05:20 +0530

    Update user.js

--- FileManager/models/user.js
+++ FileManager/models/user.js
@@ -1,24 +1,4 @@
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 15000, 
-    socketTimeoutMS: 45000,
-})
-.then(() => {
-    console.log('Connected to MongoDB Atlas');
-})
-.catch(err => {
-    console.error('MongoDB connection error:', err);
-});
-
-
-mongoose.connection.on('error', err => {
-    console.error('MongoDB connection error:', err);
-});
-
-mongoose.connection.on('disconnected', () => {
-    console.log('MongoDB disconnected');
-});
+const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     username: String,
@@ -31,7 +11,6 @@ const userSchema = mongoose.Schema({
         type: String,
         default: "default.png"
     }
-
 })
 
 module.exports = mongoose.model('user' , userSchema);
