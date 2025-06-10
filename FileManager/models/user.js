b393c087dbf8b65bb8ecff60330c7f59305d4fdf
ycode37 <ycode37@gmail.com>
2025-06-10 20:31:27 +0530

    Update user.js

--- FileManager/models/user.js
+++ FileManager/models/user.js
@@ -5,7 +5,6 @@ mongoose.connect(process.env.MONGODB_URI, {
 .then(() => {
     console.log('Connected to MongoDB Atlas');
     console.log('Connection URI:', process.env.MONGODB_URI); 
-})
 .catch(err => {
     console.error('MongoDB connection error:', err);
 });
