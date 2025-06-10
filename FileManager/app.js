5e647e73529903bdb15a1781bf40093434e60b0f
ycode37 <ycode37@gmail.com>
2025-06-10 20:24:53 +0530

    Update app.js

--- FileManager/app.js
+++ FileManager/app.js
@@ -12,6 +12,7 @@ const path = require('path');
 
 
 app.set('view engine' , 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser())
