506760b45c959a67022525c0b178704e20102252
ycode37 <ycode37@gmail.com>
2025-06-10 20:30:45 +0530

    Update user.js

--- FileManager/models/user.js
+++ FileManager/models/user.js
@@ -1,9 +1,14 @@
-const mongoose = require('mongoose');
-
-// Use the environment variable for MongoDB connection
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Connected to MongoDB Atlas');
+    console.log('Connection URI:', process.env.MONGODB_URI); 
+})
+.catch(err => {
+    console.error('MongoDB connection error:', err);
+});
 
 const userSchema = mongoose.Schema({
     username: String,
